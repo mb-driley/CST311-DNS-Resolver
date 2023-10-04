e8fa4b081a72eb9d9e331fcbfc9dc7fc2743455b
mb-driley <driley@csumb.edu>
2023-10-04 15:09:26 -0700

    Added a comments and a try, catch block

--- .idea/main.py
+++ .idea/main.py
@@ -16,10 +16,20 @@ while not check:  # While loop to continue, if check isn't updated (No valid que
     elif queryType == validQueryTypes[3:5]:  # If the query type is Type.MX
         check = True  # Updates the boolean variable to leave the while loop
 
-    else:
-        print("Invalid query type. Please try again and enter a valid query type.")
+    else:  # Else, the inputted query type isn't one of the program's valid types.
+        print("Invalid query type. Please try again and enter a valid query type.")  # Tells the user, it is not valid
 
+# print("You entered the domain: " + domainName)  # Used for testing purposes
+# print("You entered the query type: " + queryType)  # Used for testing purposes
 
-print("You entered the domain: " + domainName)  # Used for testing purposes
-print("You entered the query type: " + queryType)  # Used for testing purposes
+try:
+    search = dns.resolver.resolve(domainName, queryType)
+    for rdata in search:
+        print("Domain: " + f"{domainName}")
+        print("Query Type: " + f"{queryType}")
+        print(f"{rdata}")
+
+except Exception as e:
+    print("An error has occurred: " + {e})
+
 sys.exit(0)
