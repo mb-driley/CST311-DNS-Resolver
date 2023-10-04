22981f3f8c18631b27720e090fa013e87e485695
mb-driley <driley@csumb.edu>
2023-10-04 10:22:34 -0700

    Added prompts for User Input for a domain and query type

--- .idea/main.py
+++ .idea/main.py
@@ -1,2 +1,7 @@
+import sys
 
-
+domainName = input("Enter a domain: ")  # Prompts the user to enter a domain
+queryType = input("Enter a query type (A, NS, MX): ").upper()  # Prompts the user to enter a query type
+print("You entered the domain: " + domainName)  # Used for testing purposes
+print("You entered the query type: " + queryType)  # Used for testing purposes
+sys.exit(0)
