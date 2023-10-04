036c1925bed87777aa536eca1626b425382ac610
mb-driley <driley@csumb.edu>
2023-10-04 10:37:31 -0700

    Added another set of comments for the else statement

--- .idea/main.py
+++ .idea/main.py
@@ -16,9 +16,8 @@ while not check:  # While loop to continue, if check isn't updated (No valid que
     elif queryType == validQueryTypes[3:5]:  # If the query type is Type.MX
         check = True  # Updates the boolean variable to leave the while loop
 
-    else:
-        print("Invalid query type. Please try again and enter a valid query type.")
-
+    else:  # Else, the inputted query type isn't one of the program's valid types.
+        print("Invalid query type. Please try again and enter a valid query type.")  # Tells the user, it is not valid
 
 print("You entered the domain: " + domainName)  # Used for testing purposes
 print("You entered the query type: " + queryType)  # Used for testing purposes
