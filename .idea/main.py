0305f52a7db9012005f320033235a80e87306b70
mb-driley <driley@csumb.edu>
2023-10-04 10:29:04 -0700

    Added a boolean check variable Added a String validQueryType variable Added a while loop, so invalid queries aren't possible Added if, else if, and else statements for queries

--- .idea/main.py
+++ .idea/main.py
@@ -1,7 +1,24 @@
 import sys
 
+check = False
+validQueryTypes = "ANSMX"
 domainName = input("Enter a domain: ")  # Prompts the user to enter a domain
-queryType = input("Enter a query type (A, NS, MX): ").upper()  # Prompts the user to enter a query type
+
+while not check:
+    queryType = input("Enter a query type (A, NS, MX): ").upper()  # Prompts the user to enter a query type
+    if queryType == validQueryTypes[0]:
+        check = True
+
+    elif queryType == validQueryTypes[1:3]:
+        check = True
+
+    elif queryType == validQueryTypes[3:5]:
+        check = True
+
+    else:
+        print("Invalid query type. Please try again and enter a valid query type.")
+
+
 print("You entered the domain: " + domainName)  # Used for testing purposes
 print("You entered the query type: " + queryType)  # Used for testing purposes
 sys.exit(0)
